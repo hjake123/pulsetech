4ff105a707945f582c6b7ed793737a5726cab2b3
hyperlynx <hjake123@gmail.com>
2025-12-07 01:49:40 -0600

    Add or adjust items for controller and scanner

--- src/main/java/dev/hyperlynx/pulsetech/registration/ModItems.java
+++ src/main/java/dev/hyperlynx/pulsetech/registration/ModItems.java
@@ -16,6 +16,7 @@ public class ModItems {
     public static final DeferredItem<BlockItem> NUMBER_MONITOR = ITEMS.registerSimpleBlockItem("number_monitor", ModBlocks.NUMBER_MONITOR);
     public static final DeferredItem<BlockItem> NUMBER_EMITTER = ITEMS.registerSimpleBlockItem("number_emitter", ModBlocks.NUMBER_EMITTER);
     public static final DeferredItem<BlockItem> CONTROLLER = ITEMS.registerSimpleBlockItem("controller", ModBlocks.CONTROLLER);
+    public static final DeferredItem<BlockItem> SCANNER = ITEMS.registerSimpleBlockItem("scanner", ModBlocks.SCANNER);
 
     public static final DeferredItem<DatasheetItem> DATASHEET = ITEMS.registerItem("datasheet",  (props) ->
             new DatasheetItem(props.stacksTo(1)));
