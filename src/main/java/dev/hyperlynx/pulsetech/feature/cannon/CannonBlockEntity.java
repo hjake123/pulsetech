db485f4c1a9abc3fa04f9cd3a846341ccde97a37
hyperlynx <hjake123@gmail.com>
2025-12-19 14:47:36 -0600

    Improve Cannon breaking and add datasheet text

--- src/main/java/dev/hyperlynx/pulsetech/feature/cannon/CannonBlockEntity.java
+++ src/main/java/dev/hyperlynx/pulsetech/feature/cannon/CannonBlockEntity.java
@@ -1,23 +1,14 @@
 package dev.hyperlynx.pulsetech.feature.cannon;
 
-import dev.hyperlynx.pulsetech.Pulsetech;
 import dev.hyperlynx.pulsetech.core.protocol.ProtocolBlockEntity;
-import dev.hyperlynx.pulsetech.feature.screen.ScreenData;
-import dev.hyperlynx.pulsetech.feature.screen.ScreenUpdatePayload;
 import dev.hyperlynx.pulsetech.registration.ModBlockEntityTypes;
-import dev.hyperlynx.pulsetech.util.Color;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.NbtUtils;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.ChunkPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.state.BlockState;
-import net.neoforged.neoforge.network.PacketDistributor;
-
-import java.util.Objects;
 
 public class CannonBlockEntity extends ProtocolBlockEntity {
     private BlockPos target = getBlockPos();
@@ -30,8 +21,18 @@ public class CannonBlockEntity extends ProtocolBlockEntity {
         this.target = target;
     }
 
+    private static final int MAX_EXPLOSION_RESIST = 50;
     public void fire() {
-        level.removeBlock(target, false);
+        if(!level.isLoaded(target)) {
+            return;
+        }
+        BlockState state_to_break = level.getBlockState(target);
+        boolean can_break = state_to_break.getBlock().getExplosionResistance() < MAX_EXPLOSION_RESIST;
+        boolean can_harvest = !state_to_break.requiresCorrectToolForDrops() || !state_to_break.is(BlockTags.NEEDS_DIAMOND_TOOL);
+        if(can_break) {
+            level.destroyBlock(target, can_harvest, null);
+            level.removeBlock(target, false);
+        }
     }
 
     public void nudge(Direction direction, int amount) {
