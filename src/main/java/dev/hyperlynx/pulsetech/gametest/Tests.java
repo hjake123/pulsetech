18863f5bd3869cfd54c3a83cb24961f2358fb264
hyperlynx <hjake123@gmail.com>
2026-02-05 13:20:18 -0600

    Add test for numbers

--- src/main/java/dev/hyperlynx/pulsetech/gametest/Tests.java
+++ src/main/java/dev/hyperlynx/pulsetech/gametest/Tests.java
@@ -1,16 +1,18 @@
-package dev.hyperlynx.pulsetech.test;
+package dev.hyperlynx.pulsetech.gametest;
 
 import dev.hyperlynx.pulsetech.Pulsetech;
+import dev.hyperlynx.pulsetech.feature.number.block.NumberMonitorBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CopperBulbBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.gametest.GameTestHolder;
 
 @GameTestHolder(Pulsetech.MODID)
-public class PulsetechGameTests {
+public class Tests {
 
     @GameTest
     public static void patternBlockMatch(GameTestHelper helper) {
@@ -56,6 +58,32 @@ public class PulsetechGameTests {
                     BlockState bulb = helper.getBlockState(new BlockPos(7, 2, 0));
                     helper.assertTrue(bulb.is(Blocks.WAXED_COPPER_BULB), "Bulb was not at (7, 2, 0). The test is malformed!");
                     helper.assertFalse(bulb.getValue(CopperBulbBlock.LIT), "Detected a mismatched pattern");
+                }
+        ));
+    }
+
+    @GameTest
+    public static void number(GameTestHelper helper) {
+        helper.setBlock(0, 2, 0, Blocks.REDSTONE_BLOCK);
+        helper.runAfterDelay(40, () -> helper.succeedIf(
+                () -> {
+                    BlockEntity number_detector = helper.getBlockEntity(new BlockPos(6, 2, 0));
+                    helper.assertTrue(number_detector instanceof NumberMonitorBlockEntity, "Monitor was not at (6, 2, 0). The test is malformed!");
+                    assert number_detector instanceof NumberMonitorBlockEntity;
+                    helper.assertTrue(((NumberMonitorBlockEntity) number_detector).getNumber() == 69, "Number was [" + ((NumberMonitorBlockEntity) number_detector).getNumber() + "], should be 69");
+                }
+        ));
+    }
+
+    @GameTest
+    public static void numberRepeater(GameTestHelper helper) {
+        helper.setBlock(0, 2, 0, Blocks.REDSTONE_BLOCK);
+        helper.runAfterDelay(40, () -> helper.succeedIf(
+                () -> {
+                    BlockEntity number_detector = helper.getBlockEntity(new BlockPos(6, 2, 0));
+                    helper.assertTrue(number_detector instanceof NumberMonitorBlockEntity, "Monitor was not at (6, 2, 0). The test is malformed!");
+                    assert number_detector instanceof NumberMonitorBlockEntity;
+                    helper.assertTrue(((NumberMonitorBlockEntity) number_detector).getNumber() == 69, "Number was [" + ((NumberMonitorBlockEntity) number_detector).getNumber() + "], should be 69");
                 }
         ));
     }
